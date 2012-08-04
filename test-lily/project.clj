7b545dafd2f2a1dc4e8430131fb876d075ae7e85
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-08-04 11:26:42 +0200

    Migrate the project.clj to the lein2 version.

--- test-lily/project.clj
+++ test-lily/project.clj
@@ -1,6 +1,14 @@
-(defproject test-lily "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
+(defproject test-lily/test-lily "0.1.0-SNAPSHOT"
+  :license {:name "Eclipse Public License",
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :profiles {:dev
+             {:dependencies
+              [[com.intelie/lazytest
+                "1.0.0-SNAPSHOT"
+                :exclusions
+                [swank-clojure]]
+               [midje "1.4.0"]]}}
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :description "Testing the lily stack from the repl.")
