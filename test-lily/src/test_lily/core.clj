563c2f1d250997f21427c0aa36047e42f857dd2a
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-08-04 12:55:03 +0200

    Added the minimum command to connect to lily.

--- test-lily/src/test_lily/core.clj
+++ test-lily/src/test_lily/core.clj
@@ -1,4 +1,16 @@
 (ns test-lily.core
-  (:use [midje.sweet]))
+  (:use [midje.sweet])
+  (:import org.lilyproject.client.LilyClient))
+
+;; In the vm
+;; start the lily server
+;; ~/start-lily.sh
+
+;; Init the connection to the lily server
+(def lily-client (LilyClient. "localhost:2181" 20000))
+
+(comment (def repository (.. lily-client getRepository))
+         (def type-manager (.. lily-client getTypeManager)))
+
 
 (fact (+ 1 1) => 2)
