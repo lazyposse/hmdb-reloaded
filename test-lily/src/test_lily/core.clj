cd64ee36b98bc5178f2b2441045b0a691df1852b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-08-05 13:40:02 +0200

    Create some field type before persisting it.

--- test-lily/src/test_lily/core.clj
+++ test-lily/src/test_lily/core.clj
@@ -1,6 +1,8 @@
 (ns test-lily.core
   (:use [midje.sweet])
-  (:import org.lilyproject.client.LilyClient))
+  (:import org.lilyproject.client.LilyClient)
+  (:import org.lilyproject.repository.api.Scope)
+  (:import org.lilyproject.repository.api.QName))
 
 ;; In the vm
 ;; start the lily server
@@ -10,6 +12,17 @@
 (def lily-client (LilyClient. "localhost:2181" 20000))
 (def repository (.. lily-client getRepository))
 (def type-manager (.. repository getTypeManager))
+
+;; we will work in the namespace 'namespace-test'
+
+(def string-value-type (.. type-manager (getValueType "STRING")))
+
+;; create some field type name for the data we persist
+(def qname-title (QName. "namespace-test" "title"))
+
+;; create the field type bean (no persisting yet)
+(def field-type-title (.. type-manager (newFieldType string-value-type qname-title Scope/VERSIONED)))
+
 
 ;; This is not yet working.
 
