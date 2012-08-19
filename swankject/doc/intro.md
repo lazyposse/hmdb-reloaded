185c6d3429350e0bd77a4774b07f88fcb215b882
Denis Labaye <denis.labaye@gmail.com>
2012-08-19 03:03:35 +0200

    DONE leinification of the project as it begins to need a lot of deps

---
+++ swankject/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to swankject
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ swankject/test/swankject/core_test.clj
@@ -0,0 +1,7 @@
+(ns swankject.core-test
+  (:use clojure.test
+        swankject.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
