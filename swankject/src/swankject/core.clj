f833f26afbc941a308a11c95d4a113f7328625b9
Denis Labaye <denis.labaye@gmail.com>
2012-09-02 12:44:20 +0200

    IN-PROGRESS coding the capture in an atome

--- swankject/src/swankject/core.clj
+++ swankject/src/swankject/core.clj
@@ -1,6 +1,138 @@
-(ns swankject.core)
+;;
+;; This file contains clojure code to be evaluated when connected to the
+;; sample app.
+;;
+;;
+;; * run the sample with the agent: `make run-load-time-weaving`
+;; * then connect to the swank server with emacs `M-x slime-connect`
+;; * then you can use the forms below to test it
+;;
+(ns swankject.core
+  [:use
+   [clojure
+    [repl               :only [doc find-doc]]
+    [pprint             :only [pp pprint   ]]]
+   [clojure.tools.trace :only [trace deftrace trace-ns]]]
+  [:require
+   [clojure
+    [data                :as d]
+    [inspector           :as ins]
+    [string              :as s]
+    [test                :as t]
+    [walk                :as w]
+    [xml                 :as x]
+    [zip                 :as z]]
+   [clojure.java.javadoc :as jd]]
+  #_[:import
+     [swankject SwankjectAspect Callback CallbackImpl]
+     [sample    Main]
+     [sample.a  A]
+     [sample.b  B]])
 
-(defn -main
-  "I don't do a whole lot."
-  [& args]
-  (println "Hello, World!"))
+(ns swankjectx
+  (:use [clojure.repl   :only [doc]]))
+
+(comment (in-ns 'swankject))
+
+;; removing the callback
+(SwankjectAspect/setCallback nil)
+
+;; and runing the prog => no sign of interception should be seen
+(Main/main nil)
+
+;; now put the callback again
+(SwankjectAspect/setCallback (CallbackImpl.))
+
+;; and runing the prog => now you should see something
+(Main/main nil)
+
+;; implement a callback in clojure
+
+(def stack (atom {}))
+
+(defn before [t clazz method args]
+  (println (str "=>before " clazz "." method)))
+
+(defn- get-fn-name "hack to get the name of a fn"
+  [f] (second (s/split (clojure.main/demunge (str (getname f)))
+                       #"[/@]")))
+
+(defn- sorted-map-all "Recursively transforms nested maps into sorted nested maps. Usefull for comparing two nested datastructures"
+  [m] (w/postwalk #(if (map? %)
+                     (into (sorted-map) %)
+                     %)
+                  m))
+
+(defn- loggify "log decorator"
+  [f] (fn [& args]
+        (println (str "(" (get-fn-name f) (z/node (first args)) ", method=" (nth args 3) "...)"))
+        (let [r (apply f args)]
+          (println (str "    =>    " (with-out-str (pprint (z/root (first args))))))
+          (println)
+          r)))
+
+(defn bef
+  [cap t clazz method args]
+  (let [new-call {:tag (str clazz "." method)
+                  :attrs {:args args}}]
+    (if (z/children cap)
+      (z/right (z/insert-right (z/rightmost (z/down cap))
+                               new-call))
+      (z/down  (z/insert-child cap new-call)))))
+
+#_(def bef (loggify bef))
+
+(defn aft
+  [cap t clazz method ret]
+  (z/up (z/edit cap assoc-in [:attrs :ret] ret)))
+
+#_(def aft (loggify aft))
+
+(t/deftest itest-after-before
+  (t/is (= (z/root
+            (reduce (fn [r [fun meth arg]] (fun r nil "" meth arg))
+                    (z/xml-zip {:tag :capture})
+                    [[bef "m" "m->"     ]
+                     [bef   "a" "a->"   ]
+                     [bef     "s" "s->" ]
+                     [aft     "s" "<-s" ]
+                     [aft   "a" "<-a"   ]
+                     [bef   "b" "b->"   ]
+                     [aft   "b" "<-b"   ]
+                     [aft "m" "<-m"     ]]))
+           {:tag     :capture
+            :content [{:tag     ".m"
+                       :attrs   {:args "m->"
+                                 :ret  "<-m"}
+                       :content [{:tag     ".a"
+                                  :attrs   {:args "a->"
+                                            :ret  "<-a"}
+                                  :content [{:tag ".s"
+                                             :attrs {:args "s->"
+                                                     :ret  "<-s"}}]}
+                                 {:tag     ".b"
+                                  :attrs   {:args "b->"
+                                            :ret  "<-b"}}]}]})))
+
+(defn pz [loc]
+  (println "current node:")
+  (pp/pprint (z/node loc))
+  (println "whole: ")
+  (pp/pprint (z/root loc)))
+
+
+
+
+(def p
+  (proxy [Callback] []
+    (before         [t clazz method args     ] (before t clazz method args))
+    (afterReturning [t clazz method ret      ] (println (str "<=afterReturning " clazz "." method)))
+    (afterThrowing  [t clazz method throwable] (println (str "<=afterThrowing " clazz "." method)))))
+
+;; set it
+
+(SwankjectAspect/setCallback p)
+
+;; see it in action
+(Main/main nil)
+
