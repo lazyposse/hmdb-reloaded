c76604af193fa777050f3ae7083bc6f479ef0f61
Denis Labaye <denis.labaye@gmail.com>
2012-09-02 13:27:32 +0200

    DONE the basic capture of methods call in an atom is working

--- swankject/src/swankject/core.clj
+++ swankject/src/swankject/core.clj
@@ -1,3 +1,4 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;; This file contains clojure code to be evaluated when connected to the
 ;; sample app.
@@ -7,6 +8,7 @@
 ;; * then connect to the swank server with emacs `M-x slime-connect`
 ;; * then you can use the forms below to test it
 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (ns swankject.core
   [:use
    [clojure
@@ -23,16 +25,15 @@
     [xml                 :as x]
     [zip                 :as z]]
    [clojure.java.javadoc :as jd]]
-  #_[:import
-     [swankject SwankjectAspect Callback CallbackImpl]
-     [sample    Main]
-     [sample.a  A]
-     [sample.b  B]])
+  [:import
+   [swankject SwankjectAspect Callback CallbackImpl]
+   [sample    Main]
+   [sample.a  A]
+   [sample.b  B]])
 
-(ns swankjectx
-  (:use [clojure.repl   :only [doc]]))
-
-(comment (in-ns 'swankject))
+;;-----------------------------------------------------------------------------
+;; Checking that we can manipulate the app from the repl
+;;-----------------------------------------------------------------------------
 
 ;; removing the callback
 (SwankjectAspect/setCallback nil)
@@ -46,24 +47,41 @@
 ;; and runing the prog => now you should see something
 (Main/main nil)
 
-;; implement a callback in clojure
+;;-----------------------------------------------------------------------------
+;; Implements a simple callback in clojure
+;;-----------------------------------------------------------------------------
 
-(def stack (atom {}))
+(def simple-log-callback
+  (proxy [Callback] []
+    (before         [t clazz method args     ] (println (str "before====>      " clazz "." method)))
+    (afterReturning [t clazz method ret      ] (println (str "<=afterReturning " clazz "." method)))
+    (afterThrowing  [t clazz method throwable] (println (str "<=afterThrowing  " clazz "." method)))))
 
-(defn before [t clazz method args]
-  (println (str "=>before " clazz "." method)))
+;; set it
+(SwankjectAspect/setCallback simple-log-callback)
 
-(defn- get-fn-name "hack to get the name of a fn"
-  [f] (second (s/split (clojure.main/demunge (str (getname f)))
+;; see it in action
+(Main/main nil)
+
+;;-----------------------------------------------------------------------------
+;; Implements a callback to records the method calls
+;;-----------------------------------------------------------------------------
+
+(defn- get-fn-name
+  "For dev: hack to get the name of a fn"
+  [f] (second (s/split (clojure.main/demunge (str f))
                        #"[/@]")))
 
-(defn- sorted-map-all "Recursively transforms nested maps into sorted nested maps. Usefull for comparing two nested datastructures"
+(defn- sorted-map-all
+  "For dev: Recursively transforms nested maps into sorted nested maps.
+Usefull for comparing two nested datastructures"
   [m] (w/postwalk #(if (map? %)
                      (into (sorted-map) %)
                      %)
                   m))
 
-(defn- loggify "log decorator"
+(defn- loggify
+  "For dev: log decorator"
   [f] (fn [& args]
         (println (str "(" (get-fn-name f) (z/node (first args)) ", method=" (nth args 3) "...)"))
         (let [r (apply f args)]
@@ -72,6 +90,8 @@
           r)))
 
 (defn bef
+  "Return the new value for the capture, after 'before' has been called.
+Must start with the value: (z/xml-zip {:tag :capture})"
   [cap t clazz method args]
   (let [new-call {:tag (str clazz "." method)
                   :attrs {:args args}}]
@@ -83,6 +103,7 @@
 #_(def bef (loggify bef))
 
 (defn aft
+  "Return the new value for the capture, after 'after' has been called"
   [cap t clazz method ret]
   (z/up (z/edit cap assoc-in [:attrs :ret] ret)))
 
@@ -114,25 +135,21 @@
                                   :attrs   {:args "b->"
                                             :ret  "<-b"}}]}]})))
 
-(defn pz [loc]
-  (println "current node:")
-  (pp/pprint (z/node loc))
-  (println "whole: ")
-  (pp/pprint (z/root loc)))
+(def capture (atom (z/xml-zip {:tag :capture})))
 
-
-
-
-(def p
+(def capture-callback
   (proxy [Callback] []
-    (before         [t clazz method args     ] (before t clazz method args))
-    (afterReturning [t clazz method ret      ] (println (str "<=afterReturning " clazz "." method)))
-    (afterThrowing  [t clazz method throwable] (println (str "<=afterThrowing " clazz "." method)))))
+    (before         [t clazz method args     ] (swap! capture bef t clazz method args) )
+    (afterReturning [t clazz method ret      ] (swap! capture aft t clazz method ret))
+    (afterThrowing  [t clazz method throwable] (println (str "<=afterThrowing  (not implemented)" clazz "." method)))))
 
 ;; set it
+(SwankjectAspect/setCallback capture-callback)
 
-(SwankjectAspect/setCallback p)
-
-;; see it in action
-(Main/main nil)
-
+(comment
+  ;; run it
+  (Main/main nil)
+  ;; display the content of the atom:
+  (pprint @capture)
+  ;; display as XML:
+  (println (x/emit (z/root @capture))))
